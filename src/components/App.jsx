93f5fdc7cd3414e6bacc8a32434690ea730c4ca7
AnnBortnik <libra.merie.panda@gmail.com>
2023-12-27 13:25:19 +0100

    general update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import React from 'react';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json'; 
+import TransactionHistory from './transactionhistory/Transactionhistory'; 
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import FriendList from './friendlist/Friendlist';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} /> 
+      <FriendList friends={friends} /> 
+      <TransactionHistory items={transactions} /> 
+    </>
   );
 };
